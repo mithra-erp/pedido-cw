7f33477702ec827f87d89bd1e42f37b6c3132f84
Vanderson Rabelo <v.rabelo1@hotmail.com>
2024-04-22 20:52:36 -0300

    teste

--- script.js
+++ script.js
@@ -178,8 +178,6 @@ const __save = () => {
 }
 
 const __main = () => {
-    let test = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvYXBpLm1pdGhyYS5jb20uYnJcL21hbmFnZXIiLCJpYXQiOjE3MTM4MjYyMTksImV4cCI6MTcxMzgyOTgxOSwiYXVkIjoiOTI3MDZiYTQtZmQzMC0yMmNlLWRlNmQtMTYxMGIxN2EwZDJkIiwidUlkIjoibWl0aHJhIiwicm9sZXMiOlsiQURNSU4iXX0.wjFfD7mwZOcyw7EzI3waoF9w7XFgDj3-01vfP94kXS8"
-    sessionStorage.setItem('access_token', test);
     __getFiliais();
 }
 
